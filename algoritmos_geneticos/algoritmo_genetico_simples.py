ca39096fcfbf9652ed2a50823fe517a08e1c4f52
eduardohscosta <eduhenrique93@gmail.com>
2022-05-14 20:29:46 -0300

    Add main fail of a GA

---
+++ algoritmos_geneticos/algoritmo_genetico_simples.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+Script principal que executa os 3 principais operadores de um Algoritmo Genético simples.
+
+(c) Eduardo Henrique Silveira da Costa (2020)
+
+Monografia apresentada como requisito à obtenção do grau de Bacharel em Ciências Econômicas
+pelo Curso de Ciências Econômicas, Setor de Ciências Sociais Aplicada,
+Universidade Federal do Paraná (UFPR).
+
+Problema:
+
+- Problema de Valor Máximo em Uma Caixa Preta (Da Costa, 2022)
+
+Definicao do Problema:
+
+- Dada uma Caixa Preta, contendo 5 interruptores com um sinal de entrada
+- de 0 ou 1 (Desligado ou Ligado), defina a configuração de interruptores
+- com o maior sinal de saída (recompensa), através da maximização da fun-
+- cao objetivo f(x) = x^2, onde x pode apresentar qualquer inteiro no
+- intervalo [0, 31].
+
+FONTE: Adaptado de Goldberg (1889)
+
+This project is licensed under the terms of the MIT license.
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
